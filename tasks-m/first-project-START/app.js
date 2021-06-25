cef1f92d176f77859582c2b66b71a6f83ce2ad8b
elena_kau <lelickau@yandex.ru>
2021-06-25 17:10:45 +0200

    first commit

---
+++ tasks-m/first-project-START/app.js
@@ -0,0 +1,122 @@
+const tasks = [
+  {
+    _id: '5d2ca9e2e03d40b326596aa7',
+    completed: true,
+    body:
+      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+    title: 'Eu ea incididunt sunt consectetur fugiat non.',
+  },
+  {
+    _id: '5d2ca9e29c8a94095c1288e0',
+    completed: false,
+    body:
+      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+    title:
+      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+  },
+  {
+    _id: '5d2ca9e2e03d40b3232496aa7',
+    completed: true,
+    body:
+      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+    title: 'Eu ea incididunt sunt consectetur fugiat non.',
+  },
+  {
+    _id: '5d2ca9e29c8a94095564788e0',
+    completed: false,
+    body:
+      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+    title:
+      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+  },
+];
+
+(function(arrOfTasks) {
+
+  //1 переводим массив тасков в объект объектов:
+  const objOfTask = arrOfTasks.reduce((acc, task) => {
+    acc[task._id] = task;
+    return acc;
+  }, {});
+
+  //получим список куда добавим фрагиент кода сразметкой
+  const listContainer = document.querySelector('.tasks-list-section .list-group');
+
+  //получаем форму
+  const form = document.forms['addTask'];
+  const inputTitle = form.elements['title'];
+  const inputBody = form.elements['body'];
+
+  //2 функция вывода задачь на страницу
+  const renderAllTask = (tasksList) => {
+    //проверка наличия переданного списка
+    if (!tasksList) {
+      console.error('not list')
+      return
+    }
+    //создаем фрагмент который будем выгружать на страницу
+    const fragment = document.createDocumentFragment();
+            //переберем объект вернув его массивом
+    Object.values(tasksList).forEach(task => {
+      const li = listItemTemplate(task);
+      fragment.appendChild(li);
+    })
+    listContainer.appendChild(fragment);
+  }
+
+  //4 функц которая создаст элемент li
+  function listItemTemplate({_id, title, body} = {}) {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2');
+    
+    const span = document.createElement('span');
+    span.textContent = title;
+    span.style.fontWeight = 'bold';
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('btn', 'btn-danger', 'ml-auto', 'delete-btn');
+    deleteBtn.textContent = 'Delete';
+
+    const article = document.createElement('p');
+    article.textContent = body;
+    article.classList.add('mt-2', 'w-100');
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    li.appendChild(article);
+
+    return li
+  }
+  //5 обработка события отправки формы
+  function onFormSubmit(e) {
+    e.preventDefault();
+    const titleVal = inputTitle.value;
+    const bodyVal = inputBody.value;
+    if(!titleVal || !bodyVal) {
+      alert('text the message');
+      return
+    }
+    const task = createNewTask(titleVal, bodyVal);
+    const listItem = listItemTemplate(task);
+    listContainer.insertAdjacentElement('afterbegin', listItem);
+    form.reset();
+  }
+
+  //6 функ создания новой задачи и добавление ее в основной объект с задачами
+  function createNewTask(title, body) {
+    const newTask = {
+      title, 
+      body,
+      completed: false,
+      _id: `task-${Math.floor((Math.random())*1000000)}-${Math.floor((Math.random())*10000000)}`,
+    }
+    objOfTask[newTask._id] = newTask;
+    return {...newTask}
+  }
+
+  //вызовы 
+  renderAllTask(objOfTask);
+  form.addEventListener('submit', onFormSubmit);
+
+
+})(tasks);
