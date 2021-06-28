159029e26e305eb9c919e9f89de7463c109b7156
elena_kau <lelickau@yandex.ru>
2021-06-28 15:26:55 +0200

    delete, theme, localtorage

--- tasks-m/app.js
+++ tasks-m/app.js
@@ -39,6 +39,81 @@ const tasks = [
     return acc;
   }, {});
 
+  //темы
+  const themes = {
+    default: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#007bff',
+      '--header-text-color': '#fff',
+      '--default-btn-bg': '#007bff',
+      '--default-btn-text-color': '#fff',
+      '--default-btn-hover-bg': '#0069d9',
+      '--default-btn-border-color': '#0069d9',
+      '--danger-btn-bg': '#dc3545',
+      '--danger-btn-text-color': '#fff',
+      '--danger-btn-hover-bg': '#bd2130',
+      '--danger-btn-border-color': '#dc3545',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#80bdff',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(0, 123, 255, 0.25)',
+    },
+    dark: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#343a40',
+      '--header-text-color': '#fff',
+      '--default-btn-bg': '#58616b',
+      '--default-btn-text-color': '#fff',
+      '--default-btn-hover-bg': '#292d31',
+      '--default-btn-border-color': '#343a40',
+      '--default-btn-focus-box-shadow':
+        '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+      '--danger-btn-bg': '#b52d3a',
+      '--danger-btn-text-color': '#fff',
+      '--danger-btn-hover-bg': '#88222c',
+      '--danger-btn-border-color': '#88222c',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#78818a',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+    },
+    light: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#fff',
+      '--header-text-color': '#212529',
+      '--default-btn-bg': '#fff',
+      '--default-btn-text-color': '#212529',
+      '--default-btn-hover-bg': '#e8e7e7',
+      '--default-btn-border-color': '#343a40',
+      '--default-btn-focus-box-shadow':
+        '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+      '--danger-btn-bg': '#f1b5bb',
+      '--danger-btn-text-color': '#212529',
+      '--danger-btn-hover-bg': '#ef808a',
+      '--danger-btn-border-color': '#e2818a',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#78818a',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+    },
+  };
+
+  // предыдущее состояние селектора выбора темы (для случая отмены изменения темы)
+  let lastSelectedTheme =  localStorage.getItem('app_theme') || 'default';
+  setTheme(lastSelectedTheme);
+
+  //получаем чекбокс для установки темы
+  const themeSelect = document.querySelector('#themeSelect');
+
   //получим список куда добавим фрагиент кода сразметкой
   const listContainer = document.querySelector('.tasks-list-section .list-group');
 
@@ -54,7 +129,7 @@ const tasks = [
       console.error('not list')
       return
     }
-    //создаем фрагмент который будем выгружать на страницу
+    //3 создаем фрагмент который будем выгружать на страницу
     const fragment = document.createDocumentFragment();
             //переберем объект вернув его массивом
     Object.values(tasksList).forEach(task => {
@@ -68,6 +143,8 @@ const tasks = [
   function listItemTemplate({_id, title, body} = {}) {
     const li = document.createElement('li');
     li.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2');
+
+    li.setAttribute('data-task-id', _id);
     
     const span = document.createElement('span');
     span.textContent = title;
@@ -114,9 +191,54 @@ const tasks = [
     return {...newTask}
   }
 
+//7 удаление постов через делигирование (через родительский элемент)
+  function onDeleteHandler({target}){
+    if(target.classList.contains('delete-btn')) {
+      const parent = target.closest('[data-task-id]');
+      const id = parent.dataset.taskId;
+      
+      const confirmed = deleteTask(id);
+      deleteTaskFromHTML(confirmed, parent);
+    }
+  }
+
+  function deleteTask( id) {
+    const {title} = objOfTask[id];
+    const isConfirm = confirm(`Do you wont delete post '${title}'?`);
+    if(!isConfirm) return isConfirm;
+    delete objOfTask[id];
+    return isConfirm;
+  }
+
+  function deleteTaskFromHTML(confirm, el) {
+    if(!confirm) return;
+    el.remove();
+  }
+
+  // 8 функции для тем
+  function onThemeSelectHandler(e) {
+    const selectedTheme = themeSelect.value;
+    const isConfirm = confirm(`To change the theme to ${selectedTheme}`);
+    if(!isConfirm) {
+      themeSelect.value = lastSelectedTheme;
+      return;
+    };
+    setTheme(selectedTheme);
+    lastSelectedTheme = selectedTheme;
+    localStorage.setItem('app_theme', selectedTheme);
+  }
+
+  function setTheme(name) {
+    const selectedThemeObj = themes[name];
+    Object.entries(selectedThemeObj).forEach(([key, value]) => {
+      document.documentElement.style.setProperty(key, value);
+    })
+  }
+
   //вызовы 
   renderAllTask(objOfTask);
   form.addEventListener('submit', onFormSubmit);
-
+listContainer.addEventListener('click', onDeleteHandler);
+themeSelect.addEventListener('change', onThemeSelectHandler);
 
 })(tasks);
